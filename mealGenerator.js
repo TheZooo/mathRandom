e79c64a34947d28f3b6ebecd9f7eaf1eb32a323a
Eric Zhou <42453465+TheZooo@users.noreply.github.com>
2018-08-21 13:00:25 -1000

    Added more things to the array.

--- mealGenerator.js
+++ mealGenerator.js
@@ -1,5 +1,5 @@
-var x = ["Stirfry", "Fried", "Pan-cooked", "Steamed", "Grilled", "Slow-cooked", "Baked", "Broiled", "Minced", "BBQ"];
-var y = ["Steak", "Pork", "Fish", "Potato", "Onions", "Clams", "Rice", "Hams", "Egg", "Carrot", "Tomato", "Chicken"];
+var x = ["Stirfry", "Fried", "Pan-cooked", "Steamed", "Grilled", "Slow-cooked", "Baked", "Broiled", "Minced", "BBQ", "Glazed", "Charred"];
+var y = ["Steak", "Pork", "Fish", "Potato", "Onions", "Clams", "Rice", "Hams", "Egg", "Carrot", "Tomato", "Chicken", "Lettuce", "Squid"];
 var a = Math.floor(Math.random() * x.length);
 var b = Math.floor(Math.random() * x.length);
 var c = Math.floor(Math.random() * y.length);
