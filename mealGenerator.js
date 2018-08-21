428e4e2bada81b0ccf7980f4b7ef0d0f9e8e6f54
Eric Zhou <42453465+TheZooo@users.noreply.github.com>
2018-08-21 12:57:14 -1000

    Add files via upload

---
+++ mealGenerator.js
@@ -0,0 +1,7 @@
+var x = ["Stirfry", "Fried", "Pan-cooked", "Steamed", "Grilled", "Slow-cooked", "Baked", "Broiled", "Minced", "BBQ"];
+var y = ["Steak", "Pork", "Fish", "Potato", "Onions", "Clams", "Rice", "Hams", "Egg", "Carrot", "Tomato", "Chicken"];
+var a = Math.floor(Math.random() * x.length);
+var b = Math.floor(Math.random() * x.length);
+var c = Math.floor(Math.random() * y.length);
+var d = Math.floor(Math.random() * y.length);
+window.alert(x[a] + " " + y[c] + " " + "with" + " " + x[b] + " " + y[d]);
